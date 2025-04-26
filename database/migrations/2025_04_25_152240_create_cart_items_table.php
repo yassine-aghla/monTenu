06a863fb4373cf1802de56a9da819ab16c4f8cc7
yassine <yassineaghla23@gmail.com>
2025-04-26 18:37:18 +0100

    modifier la migration pour cart et ajoute la migration pour cart items et faire les models nesseaires

---
+++ database/migrations/2025_04_25_152240_create_cart_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+        $table->foreignId('cart_id')->constrained()->onDelete('cascade');
+        $table->foreignId('tenue_id')->constrained()->onDelete('cascade');
+        $table->integer('quantity')->default(1);
+        $table->timestamps();
+        $table->unique(['cart_id', 'tenue_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
