54886c9de5f6a0801c7ee90d6c7afead5fac24e6
yassine <yassineaghla23@gmail.com>
2025-05-06 14:15:38 +0100

    faire le model pour le paiment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'payment_id',
+        'payment_method',
+        'amount',
+        'currency',
+        'status',
+        
+    ];
+  
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+ 
+}
